17a409c1f175b2ae593287abd69706ce3a0b562f
shivaprasad-commits <shivaprasad.b@orotron.com>
2025-08-21 16:42:37 +0530

    updated the schemas projects and user_projects and users

--- src/db/schema/userProjects.ts
+++ src/db/schema/userProjects.ts
@@ -1,19 +1,19 @@
 import { index, integer, pgTable, serial, timestamp } from "drizzle-orm/pg-core";
 
-import { projects } from "./project";
-import { users } from "./users";
+import { projects } from "./project.js";
+import { users } from "./users.js";
 
 export const user_projects = pgTable("user_projects", {
-  id: serial().primaryKey(),
-  project_id: integer().references(() => projects.id),
-  user_id: integer().references(() => users.id),
-  created_at: timestamp().defaultNow(),
-  updated_at: timestamp(),
-  deleted_at: timestamp(),
+    id: serial().primaryKey(),
+    project_id: integer().references(() => projects.id),
+    user_id: integer().references(() => users.id),
+    created_at: timestamp().defaultNow(),
+    updated_at: timestamp(),
+    deleted_at: timestamp(),
 }, t => [
-  index("user_projects_id_idx").on(t.id),
-  index("user_projects_project_id_idx").on(t.project_id),
-  index("user_projects_user_id_idx").on(t.user_id),
+    index("user_projects_id_idx").on(t.id),
+    index("user_projects_project_id_idx").on(t.project_id),
+    index("user_projects_user_id_idx").on(t.user_id),
 
 ]);
 
