f2138da5d7f0cbe81ee06352152f6edb62d2d38a
Deepthinaiduchappa <deepthinaiduchappa2154@gmail.com>
2025-09-04 15:03:02 +0530

    --DP Added api for get tasks by project id

--- src/routes/taskAssigneesRoutes.ts
+++ src/routes/taskAssigneesRoutes.ts
@@ -9,7 +9,7 @@ const taskAssigneesController = new TaskAssigneesController();
 // taskAssigneesRoutes.delete("/:id/assignees", taskAssigneesController.deleteTaskAssignee);
 taskAssigneesRoutes.delete("/:id/assignees", taskAssigneesController.removeAssigneesByTaskId);
 taskAssigneesRoutes.delete("/:id", taskAssigneesController.deleteTask);
-//taskAssigneesRoutes.get("/:id", taskAssigneesController.getTasksByProjectId);
+taskAssigneesRoutes.get("/:id", taskAssigneesController.getTasksByProjectId);
 taskAssigneesRoutes.get("/:id/users", taskAssigneesController.getAssigneesByTaskId);
 taskAssigneesRoutes.post("/:id/assignees", taskAssigneesController.addUsersToTask);
 taskAssigneesRoutes.post("/", isEmployeAuthorized, taskAssigneesController.createTask);
