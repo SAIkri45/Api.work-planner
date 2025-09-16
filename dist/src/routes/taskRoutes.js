64df3e8aabefe8bf4b3ff5b4505eab664a03bf2d
shivaprasad-commits <shivaprasad.b@orotron.com>
2025-09-16 14:04:08 +0530

    SP--UPDATE:Update update task api

--- dist/src/routes/taskRoutes.js
+++ dist/src/routes/taskRoutes.js
@@ -1,9 +1,10 @@
 import { Hono } from "hono";
 import TasksController from "../controllers/taskControllers.js";
+import { isAuthorized, isManagerOrAdmin } from "../middlewares/isAuthorized.js";
 const taskRoutes = new Hono();
 const tasksController = new TasksController();
 taskRoutes.get("/status/counts", tasksController.getTaskStatusCounts);
 taskRoutes.get("/:id", tasksController.getTaskById);
-taskRoutes.patch("/:id", tasksController.editTask);
+taskRoutes.patch("/:id", isManagerOrAdmin, isAuthorized, tasksController.editTask);
 taskRoutes.get("/", tasksController.getPaginatedTasks);
 export default taskRoutes;
