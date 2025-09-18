833e6d21bc8c687b069c418bcac30b3aea6de1bd
shivaprasad-commits <shivaprasad.b@orotron.com>
2025-09-18 08:30:04 +0530

    SP--UPDATE:Updated the routes with middleware

--- dist/src/middlewares/isAuthorized.js
+++ dist/src/middlewares/isAuthorized.js
@@ -6,27 +6,6 @@ const isAuthorized = createMiddleware(async (c, next) => {
     c.set("user_payload", userDetails);
     await next();
 });
-const isOptionalAuthorized = createMiddleware(async (c, next) => {
-    const isPublic = c.req.query("is_public") || "false";
-    if (isPublic && isPublic === "true") {
-        await next();
-    }
-    else {
-        const userDetails = await getUserDetailsFromToken(c);
-        c.set("user_payload", userDetails);
-        await next();
-    }
-});
-// const isManagerOrAdmin = createMiddleware(async (c: Context, next) => {
-//   const userDetails = await getUserDetailsFromToken(c);
-//   if (userDetails.user_type === "ADMIN" || userDetails.user_type === "MANAGER") {
-//     c.set("user_payload", userDetails);
-//     await next();
-//   }
-//   else {
-//     return sendSuccessResp(c, 401, "Access denied to create project");
-//   }
-// });
 const isManagerOrAdmin = createMiddleware(async (c, next) => {
     try {
         const userDetails = await getUserDetailsFromToken(c);
@@ -42,4 +21,4 @@ const isManagerOrAdmin = createMiddleware(async (c, next) => {
         throw error;
     }
 });
-export { isAuthorized, isManagerOrAdmin, isOptionalAuthorized };
+export { isAuthorized, isManagerOrAdmin };
--- src/middlewares/isAuthorized.ts
+++ src/middlewares/isAuthorized.ts
@@ -11,29 +11,6 @@ const isAuthorized = createMiddleware(async (c: Context, next) => {
   await next();
 });
 
-const isOptionalAuthorized = createMiddleware(async (c: Context, next) => {
-  const isPublic = c.req.query("is_public") || "false";
-  if (isPublic && isPublic === "true") {
-    await next();
-  }
-  else {
-    const userDetails = await getUserDetailsFromToken(c);
-    c.set("user_payload", userDetails);
-    await next();
-  }
-});
-
-// const isManagerOrAdmin = createMiddleware(async (c: Context, next) => {
-//   const userDetails = await getUserDetailsFromToken(c);
-//   if (userDetails.user_type === "ADMIN" || userDetails.user_type === "MANAGER") {
-//     c.set("user_payload", userDetails);
-//     await next();
-//   }
-//   else {
-//     return sendSuccessResp(c, 401, "Access denied to create project");
-//   }
-// });
-
 const isManagerOrAdmin = createMiddleware(async (c: Context, next) => {
   try {
     const userDetails = await getUserDetailsFromToken(c);
@@ -51,4 +28,4 @@ const isManagerOrAdmin = createMiddleware(async (c: Context, next) => {
   }
 });
 
-export { isAuthorized, isManagerOrAdmin, isOptionalAuthorized };
+export { isAuthorized, isManagerOrAdmin };
