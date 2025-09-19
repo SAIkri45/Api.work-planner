a75b55ccbbad63c234372c6e1abc5d818b5098ce
shivaprasad-commits <shivaprasad.b@orotron.com>
2025-09-19 10:21:14 +0530

    SP--NEW:UPDATED:Updated the task  routes

--- dist/src/routes/taskRoutes.js
+++ dist/src/routes/taskRoutes.js
@@ -1,11 +1,11 @@
 import { Hono } from "hono";
 import TasksController from "../controllers/taskControllers.js";
-import { isAuthorized, isManagerOrAdmin } from "../middlewares/isAuthorized.js";
+import { isAuthorized } from "../middlewares/isAuthorized.js";
 const taskRoutes = new Hono();
 const tasksController = new TasksController();
 taskRoutes.get("/status/counts", tasksController.getTaskStatusCounts);
-taskRoutes.patch("/:id/status", isManagerOrAdmin, tasksController.updateTaskStatus);
+taskRoutes.patch("/:id/status", isAuthorized, tasksController.updateTaskStatus);
 taskRoutes.get("/:id", isAuthorized, tasksController.getTaskById);
-taskRoutes.patch("/:id", isManagerOrAdmin, tasksController.editTask);
+taskRoutes.patch("/:id", isAuthorized, tasksController.editTask);
 taskRoutes.get("/", isAuthorized, tasksController.getPaginatedTasks);
 export default taskRoutes;
