1e4611cacf5ec8e5535b8842dd76810fbeae0fde
madhuri-oro <madhuri.c@orotron.com>
2025-10-16 07:37:05 +0530

    modified mark all read notifications api

--- src/routes/notificationRoutes.ts
+++ src/routes/notificationRoutes.ts
@@ -6,9 +6,11 @@ import { isAuthorized } from "../middlewares/isAuthorized.js";
 const notificationRoute = new Hono();
 const notificationController = new NotificationController();
 
+
+
 notificationRoute.get("/", isAuthorized, notificationController.getNotifications);
 notificationRoute.patch("/:id", isAuthorized, notificationController.isNotificationRead);
-notificationRoute.put("/", isAuthorized, notificationController.markAllNotificationsRead);
+notificationRoute.put("/mark-as-read/all", isAuthorized, notificationController.markAllNotificationsRead);
 notificationRoute.get("/unread-notifications-count", isAuthorized, notificationController.countUnreadNotifications);
 
 export default notificationRoute;
